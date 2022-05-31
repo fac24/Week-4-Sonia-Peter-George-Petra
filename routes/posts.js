b72908a433bfedeb8b6c0d91e1fef026f2540b65
glombort <george.lambert99@gmail.com>
2022-05-31 16:12:02 +0100

    Debugged
    Co-Authored-By: Sonia <82713219+sonianb@users.noreply.github.com>
    Co-Authored-By: Peter James Salter <45575016+PJSalter@users.noreply.github.com>
    Co-Authored-By: Petra <85565743+alternadiva@users.noreply.github.com>

--- routes/posts.js
+++ routes/posts.js
@@ -1,41 +1,44 @@
 const model = require("../database/model.js");
-
+const layout = require("../layout.js");
 
 async function get(request, response) {
-   const posts = await model.getPosts()
-   if(posts.length === 0) {
-       console.log("post me")
-    response.send("<p>Be the first one to post!</p>")
-   } else {
-        let postsHTML = ""; 
-        const sid = request.signedCookies.sid;
-        const userData = await model.getSession(sid);
+  const posts = await model.getPosts();
+  if (posts.length === 0) {
+    console.log("post me");
+    response.send(layout("posts", "<p>Be the first one to post!</p>"));
+  } else {
+    let postsHTML = "";
+    const sid = request.signedCookies.sid;
+    const userData = await model.getSession(sid);
 
-        posts.map((post) => {
-            console.log(post)
-            let deleteButton = "";
-            // if the posts equals the array of objects with data most needed.
-            if(post.user_id === userData.user.id) {
-                console.log("I")
-            deleteButton = /*html*/ ` 
-                <form action="/delete-post" method="POST">
-                    <button class="delete-button" name="post_id" value="${post.id}" aria-label="Delete ${post.post}">
-                    &times;
-                    </button>
-                </form>`;
-            }
+    posts.map((post) => {
+      console.log(post);
+      let deleteButton = "";
+      // if the posts equals the array of objects with data most needed.
+      if (post.user_id === userData.id) {
+        console.log("I");
+        deleteButton = /*html*/ ` 
+          <form action="/delete-post" method="POST">
+            <button class="delete-button" name="post_id" value="${post.id}" aria-label="Delete ${post.post}">
+              &times;
+            </button>
+          </form>
+        `;
+      }
 
-            let postHTML = `
-            <div>
-            <p>User: ${post.email}</p>
-            <p>recipe: ${post.recipe}</p>
-            <p>jokes: ${post.joke}</p>
-            <img>food photo: ${post.photo}</img>
-            ${deleteButton}
-            `.concat(postsHTML);
-            });
-            response.send(postsHTML);
-         }
-        }
+      postsHTML += `
+        <div>
+          <p>User: ${post.email}</p>
+          <p>recipe: ${post.recipe}</p>
+          <p>jokes: ${post.joke}</p>
+          <img>food photo: ${post.photo}</img>
+          ${deleteButton}
+        </div>
+      `;
+    });
+    console.log(postsHTML);
+    response.send(layout("posts", postsHTML));
+  }
+}
 
- module.exports = {get}; 
+module.exports = { get };
