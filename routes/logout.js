0296d4343d193a9a4b4ec3a0679e29dcc5333520
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-06-01 15:13:00 +0100

    log out error handling accomplished
    co-authored-by: George <Glombort@users.noreply.github.com>
    co-authored-by: Peter <PJSalter@users.noreply.github.com>

--- routes/logout.js
+++ routes/logout.js
@@ -4,14 +4,18 @@ async function post(request, response, next) {
   try {
     const sid = request.signedCookies.sid;
     if (sid === undefined) {
-      return new Error();
+      return new Error("SID");
     }
     await deleteSession(sid);
     response.clearCookie("sid");
     response.redirect("/");
   } catch (error) {
-    console.error(error);
-    response.status(401);
+    const errorMessage = error.message;
+    if (errorMessage === "SID") {
+      error.status = 401;
+      error.message =
+        "<h1>You are unauthorised as you don't have a session ID</h1>";
+    }
     next(error);
   }
 }
